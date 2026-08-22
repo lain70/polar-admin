8b70440881fa492e3bb465e415eca4e31c35a288
polarbear <encomium89@gmail.com>
2026-08-22 14:11:03 +0900

    admin: run dev server on port 9100

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Polar Bear Shop 관리자용 Vue 2 애플리케이션입니다.
 ## 실행
 
 - `npm install`
-- `npm start`: 로컬 개발 서버를 9001 포트에서 실행
+- `npm start`: 로컬 개발 서버를 9100 포트에서 실행
 - `npm run build`: 운영 번들 생성
 - `npm run lint -- --no-fix`: ESLint 검사
 
--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
   publicPath: process.env.VUE_APP_PUBLIC_PATH || undefined,
   lintOnSave: false,
   devServer: {
-    port: 9001,
+    port: 9100,
     proxy: {
       '^/api': {
         target: 'http://localhost:9090/api'
